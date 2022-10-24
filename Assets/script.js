4c3342ca70e53f483b3d87ab45692225f3ab58bb
Vlada <111080921+VladaBolotova@users.noreply.github.com>
2022-10-24 17:52:45 -0400

    adding submit button

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,10 @@
 var timeLeft=75;
+var submitButton=document.getElementById("submit");
+var formElement=document.getElementById("name");
+var form=document.getElementById("initials");
+var infoElement=document.getElementsByClassName("info-box");
 var buttonElement=document.getElementById("btn");
+var infoBox=document.getElementById("info");
 var questionElement = document.getElementById("questionDisplay");
 var currentIndex = 0;
  var questions=["What does HTML stand for?","What does CSS stand for?","Where should a CSS be referenced in a HTML file?","Which of these is a genuine tag keyword?"];
@@ -25,7 +30,8 @@ function timeDown(){
 }
 
 function startTimer(){
-    buttonElement.remove();
+//forEach//
+    infoBox.remove(), 
     clockMedia=setInterval(timeDown, 1000)
     makeQuestion();
 }
@@ -33,14 +39,21 @@ function startTimer(){
 function checkOption(userSelected){
     console.log("Running for", userSelected)
     if ( userSelected != answers[currentIndex]){
-        timeLeft-=15;
+        timeLeft-=15,
+        alert("wrong!");
+        
+        
     }
-
+    if (userSelected == answers[currentIndex]){
+        alert("right!");
+    }
+    
     if (currentIndex < answers.length - 1){
         currentIndex++;
         makeQuestion();
     } else {
         endGame();
+       
     }
 }
 
@@ -48,24 +61,32 @@ function makeQuestion(){
     questionElement.innerHTML = `
     <div>
         <h3>${questions[currentIndex]}</h3>
-        <ul>
+        <ol>
             <li onclick="checkOption('${options[currentIndex][0]}')">${options[currentIndex][0]}</li>
             <li onclick="checkOption('${options[currentIndex][1]}')">${options[currentIndex][1]}</li>
             <li onclick="checkOption('${options[currentIndex][2]}')">${options[currentIndex][2]}</li>
             <li onclick="checkOption('${options[currentIndex][3]}')">${options[currentIndex][3]}</li>
-        </ul>
+        </ol>
     </div>
     `
    
     
 }
 
+// function submitEl(){
+//     document.getElementById("name").submit();
+//     console.log("name");
+
+// }
+
 function endGame(){
     clearInterval(clockMedia);
     questionElement.innerHTML = "End Game"
+   form.style.display="block"; 
 }
 
 
-
+// submitButton.addEventListener("click",submitEl);
 
 buttonElement.addEventListener("click", startTimer);
+
